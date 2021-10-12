25b5ce52e0b5c4c65b4b2508538f3fa998b427e8
Karthikeya Meesala <71187953+Karthikeya-Meesala@users.noreply.github.com>
2021-10-12 16:11:05 +0530

    Add files via upload

---
+++ js/firebase_configuration.js
@@ -0,0 +1,54 @@
+// Initialize Firebase
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+var config = {
+    apiKey: "AIzaSyDb5VV7bAnGv7DRJXaJxGHZemTPwk4oQZ4",
+    authDomain: "collectmails-74fd4.firebaseapp.com",
+    databaseURL: "https://collectmails-74fd4-default-rtdb.firebaseio.com/",
+    projectId: "collectmails-74fd4",
+    storageBucket: "collectmails-74fd4.appspot.com",
+    messagingSenderId: "266138946776"
+};
+firebase.initializeApp(config);
+var db = firebase.firestore();
+db.settings({
+    timestampsInSnapshots: true
+});
+
+$(document).ready(function () {
+
+    $('#notifs-form-btn').click(function (e) {
+        var is_present = false;
+        email = $('#notifs-form-input').val();
+        if (validateEmail(email)) {
+            db.collection('Emails').get().then(function (qs) {
+                qs.forEach(function (element) {
+                    if (element.data()['ID'] == email) {
+                        is_present = true;
+                    }
+                });
+
+                if (is_present == false) {
+                    db.collection("Emails").add({
+                        'ID': email,
+                    })
+                        .then(function (docRef) {
+                            console.log("Document written with ID: ", docRef.id);
+                            $("#notifs-form-btn").text("Notified!");
+                        })
+                        .catch(function (error) {
+                            console.error("Error adding document: ", error);
+                        });
+                }
+            });
+
+        }
+        else {
+            alert('Invalid Email');
+        }
+    });
+
+});
